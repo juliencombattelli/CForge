20139b334945f1eccbba1ceaba65fe2aa7d9cd32
Julien Combattelli <julien.combattelli@gmail.com>
2022-11-23 17:06:48 +0100

    Fix header length in CForgeTargetAddWarnings

--- cmake/Modules/CForgeTargetAddWarnings.cmake
+++ cmake/Modules/CForgeTargetAddWarnings.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.19)
 
 #[=======================================================================[.rst:
 CForgeTargetAddWarnings
---------
+-----------------------
 
 Add some warnings from a JSON config file to a CMake target.
 
