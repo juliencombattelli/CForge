a52a90907c816b560a7b6e76f23c43d432f3d835
Julien Combattelli <julien.combattelli@gmail.com>
2022-11-25 21:58:19 +0100

    Add CTest capability for CForgeUnit
    COMMANDs are no more supported for tests

--- cmake/Modules/CForgeUnit.cmake
+++ cmake/Modules/CForgeUnit.cmake
@@ -1,128 +1,11 @@
 cmake_minimum_required(VERSION 3.19) # For string(JSON)
 
 # CFORGE_UNIT_PROJECT: Top-level project that included CForgeUnit (analog to CMAKE_PROJECT_NAME)
-# CFORGE_UNIT_CURRENT_PROJECT: Project that most recently included CForgeUnit (analog to PROJECT_NAME)
-
 if(NOT CFORGE_UNIT_PROJECT)
     set(CFORGE_UNIT_PROJECT ${PROJECT_NAME})
 endif()
-set(CFORGE_UNIT_CURRENT_PROJECT ${PROJECT_NAME})
 
 include_guard()
-
-# TODO might be needed later on
-# function(cforge_unit_init)
-#     cmake_parse_arguments("ARG" "" "PROJECT" "" ${ARGN})
-#     if(ARG_PROJECT)
-#         if(NOT ${ARG_PROJECT}_SOURCE_DIR OR NOT ${ARG_PROJECT}_BINARY_DIR)
-#             message(FATAL_ERROR "Project ${ARG_PROJECT} is not a known project.")
-#         endif()
-#         set(CFORGE_UNIT_PROJECT ${ARG_PROJECT})
-#     endif()
-# endfunction()
-
-
-
-
-
-# JSON report generation
-# Too complexe to write using string(JSON) APIs
-# CACHE variable are used as a replacement for now
-
-# macro(_cforge_unit_report_read_file CONTENT)
-#     file(READ
-#         "${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/${CFORGE_UNIT_PROJECT}_cforge-unit-report.json"
-#         ${CONTENT}
-#     )
-# endmacro()
-
-# macro(_cforge_unit_report_write_file CONTENT)
-#     file(WRITE
-#         "${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/${CFORGE_UNIT_PROJECT}_cforge-unit-report.json"
-#         "${CONTENT}"
-#     )
-# endmacro()
-
-# function(_cforge_unit_report_create_file)
-#     if(NOT EXISTS ${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/${CFORGE_UNIT_PROJECT}_cforge-unit-report.json)
-#         message(DEBUG "CForgeUnit report file for project ${CFORGE_UNIT_PROJECT} does not exist, creating it")
-#         _cforge_unit_report_write_file("[]")
-#     else()
-#         message(DEBUG "CForgeUnit report file for project ${CFORGE_UNIT_PROJECT} already exists")
-#     endif()
-# endfunction()
-
-# macro(_cforge_unit_report_get_count CONTENT COUNT)
-#     string(JSON ${COUNT} LENGTH "${CONTENT}")
-# endmacro()
-
-# function(_cforge_unit_report_get_test_suite REPORT_FILE TEST_SUITE INDEX OBJECT)
-#     _cforge_unit_report_get_count("${REPORT_FILE}" COUNT)
-#     message(DEBUG "${COUNT} suites registered in report")
-#     if(COUNT GREATER 0)
-#         math(EXPR LAST "${COUNT} - 1")
-#         foreach(SUITE_IDX RANGE ${LAST})
-#             string(JSON SUITE_NAME GET "${REPORT_FILE}" ${SUITE_IDX} test_suite)
-#             message(DEBUG "Found suite ${SUITE_NAME} in report")
-#             if(TEST_SUITE STREQUAL SUITE_NAME)
-#                 message(DEBUG "Suite ${TEST_SUITE} found in report")
-#                 string(JSON SUITE_OBJECT GET "${REPORT_FILE}" ${SUITE_IDX})
-#                 set(${INDEX} "${SUITE_IDX}" PARENT_SCOPE)
-#                 set(${OBJECT} "${SUITE_OBJECT}" PARENT_SCOPE)
-#                 return()
-#             endif()
-#         endforeach()
-#     endif()
-# endfunction()
-
-# function(_cforge_unit_report_get_test_case TEST_SUITE_CONTENT TEST_CASE CONTENT)
-#     string(JSON COUNT LENGTH "${TEST_SUITE_CONTENT}" test_cases)
-#     message(DEBUG "${COUNT} cases registered")
-#     if(COUNT GREATER 0)
-#         math(EXPR LAST "${COUNT} - 1")
-#         foreach(CASE_IDX RANGE ${LAST})
-#             string(JSON CASE_NAME GET "${TEST_SUITE_CONTENT}" test_cases ${CASE_IDX} test_case)
-#             message(DEBUG "Found case ${CASE_NAME}")
-#             if(TEST_CASE STREQUAL CASE_NAME)
-#                 message(DEBUG "Case ${TEST_CASE} found")
-#                 string(JSON CASE_CONTENT GET "${TEST_SUITE_CONTENT}" ${CASE_IDX})
-#                 set(${CONTENT} "${CASE_CONTENT}" PARENT_SCOPE)
-#                 return()
-#             endif()
-#         endforeach()
-#     endif()
-# endfunction()
-
-# function(_cforge_unit_report_register_test TEST_SUITE TEST_CASE)
-#     _cforge_unit_report_read_file(REPORT_FILE)
-
-#     message(DEBUG "Registering suite ${TEST_SUITE}")
-
-#     _cforge_unit_report_get_test_suite("${REPORT_FILE}" "${TEST_SUITE}" SUITE_INDEX SUITE_OBJECT)
-#     if(NOT SUITE_OBJECT)
-#         _cforge_unit_report_get_count("${REPORT_FILE}" SUITE_INDEX)
-#         set(SUITE_OBJECT "{
-#             \"test_suite\": \"${TEST_SUITE}\",
-#             \"test_cases\": [],
-#         }")
-#     endif()
-
-#     message(DEBUG "Registering case ${TEST_SUITE}.${TEST_CASE}")
-
-#     _cforge_unit_report_get_test_case("${SUITE_OBJECT}" "${TEST_CASE}" CASE_OBJECT)
-#     if(NOT CASE_OBJECT)
-#         string(JSON CASE_COUNT LENGTH "${SUITE_OBJECT}" test_cases)
-#         set(CASE_OBJECT "{
-#             \"test_case\": \"${TEST_CASE}\",
-#             \"verdict_passed\": false
-#         }")
-#         string(JSON SUITE_OBJECT SET "${SUITE_OBJECT}" test_cases "${CASE_COUNT}" "${CASE_OBJECT}")
-#     endif()
-
-#     string(JSON REPORT_FILE SET "${REPORT_FILE}" "${SUITE_INDEX}" "${SUITE_OBJECT}")
-
-#     _cforge_unit_report_write_file("${REPORT_FILE}")
-# endfunction()
 
 function(_cforge_unit_report_verdict TEST_SUITE TEST_CASE VERDICT)
     set(VALID_VERDICTS "unknown;passed;failed")
@@ -165,9 +48,9 @@ endfunction()
 # TEST_SHALL_FAIL: whether the TEST_SCRIPT is expected to fail (useful if fatal error occur in test script)
 function(cforge_unit_add_test)
     cmake_parse_arguments("ARG"
-        "TEST_SHALL_FAIL"
+        "USE_CTEST;TEST_SHALL_FAIL"
         "TEST_SUITE;TEST_CASE"
-        "TEST_SCRIPT;TEST_COMMAND;VERIFY_SCRIPT;VERIFY_COMMAND"
+        "TEST_SCRIPT;VERIFY_SCRIPT"
         ${ARGN}
     )
 
@@ -177,9 +60,15 @@ function(cforge_unit_add_test)
     if(NOT ARG_TEST_CASE)
         message(FATAL_ERROR "TEST_CASE argument is required")
     endif()
-    if(NOT ARG_TEST_SCRIPT AND NOT ARG_TEST_COMMAND)
-        message(FATAL_ERROR "Either TEST_SCRIPT or TEST_COMMAND argument must be specified")
+    if(NOT ARG_TEST_SCRIPT)
+        message(FATAL_ERROR "TEST_SCRIPT argument is required")
     endif()
+
+    if(DEFINED CFORGE_UNIT_USE_CTEST)
+        set(ARG_USE_CTEST ${CFORGE_UNIT_USE_CTEST})
+    endif()
+
+    # TODO check availability of scripts
 
     if(ARG_TEST_SCRIPT)
         get_filename_component(ARG_TEST_SCRIPT "${ARG_TEST_SCRIPT}" ABSOLUTE)
@@ -189,38 +78,33 @@ function(cforge_unit_add_test)
         get_filename_component(ARG_VERIFY_SCRIPT "${ARG_VERIFY_SCRIPT}" ABSOLUTE)
     endif()
 
-    _cforge_unit_register_test(${ARG_TEST_SUITE} ${ARG_TEST_CASE})
+    set(TEST_ID "${CFORGE_UNIT_PROJECT}__${ARG_TEST_SUITE}__${ARG_TEST_CASE}")
 
     configure_file(
         ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CForgeUnitTestRunner.cmake.in
-        ${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnitTestRunner.${ARG_TEST_SUITE}.${ARG_TEST_CASE}.cmake
+        ${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnit/${TEST_ID}.cmake
         @ONLY
     )
+
+    if(ARG_USE_CTEST)
+        # Register test for deferred invokation using CTest
+        unset(CFORGE_UNIT_SUITES_${CFORGE_UNIT_PROJECT} CACHE)
+        configure_file(
+            ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CForgeUnitTestRunner.CMakeLists.txt.in
+            ${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnit/${TEST_ID}/CMakeLists.txt
+            @ONLY
+        )
+        add_test(
+            NAME "${TEST_ID}"
+            COMMAND ${CMAKE_COMMAND}
+                -S "${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnit/${TEST_ID}"
+                -B "${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnit/${TEST_ID}/build"
+        )
+    else()
+        # Register test for immediate invokation
+        _cforge_unit_register_test(${ARG_TEST_SUITE} ${ARG_TEST_CASE})
+    endif()
 endfunction()
-
-# function(cforge_unit_add_test_subdirectory DIRECTORY)
-#     cmake_parse_arguments("ARG" "USE_CTEST" "" "" ${ARGN})
-
-#     if(DEFINED CFORGE_UNIT_USE_CTEST)
-#         set(ARG_USE_CTEST ${CFORGE_UNIT_USE_CTEST})
-#     endif()
-
-#     file(REAL_PATH "${DIRECTORY}" DIRECTORY_REAL_PATH)
-#     if(NOT IS_DIRECTORY "${DIRECTORY_REAL_PATH}")
-#         message(FATAL_ERROR "Not a directory")
-#     endif()
-
-#     if(ARG_USE_CTEST)
-#         get_filename_component(FOLDER_NAME "${DIRECTORY_REAL_PATH}" NAME)
-#         add_test(
-#             NAME "${FOLDER_NAME}"
-#             COMMAND ${CMAKE_COMMAND} -S "${DIRECTORY_REAL_PATH}" -B "${CMAKE_CURRENT_BINARY_DIR}/${DIRECTORY}"
-#         )
-#         # TODO Add logic to collect test results from cache variable
-#     else()
-#         add_subdirectory(${DIRECTORY})
-#     endif()
-# endfunction()
 
 function(cforge_unit_run_tests)
     message(CHECK_START "Running all tests")
@@ -230,7 +114,7 @@ function(cforge_unit_run_tests)
         foreach(CASE IN LISTS CFORGE_UNIT_${CFORGE_UNIT_PROJECT}__${SUITE})
             list(APPEND CMAKE_MESSAGE_INDENT "  ")
             message(CHECK_START "Running test ${SUITE}.${CASE}")
-            include(${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnitTestRunner.${SUITE}.${CASE}.cmake)
+            include(${${CFORGE_UNIT_PROJECT}_BINARY_DIR}/CForgeUnit/${CFORGE_UNIT_PROJECT}__${SUITE}__${CASE}.cmake)
             _cforge_unit_test_fn()
             if(CFORGE_UNIT_VERDICT_${CFORGE_UNIT_PROJECT}__${SUITE}__${CASE} STREQUAL failed)
                 message(CHECK_FAIL "failed")
