2070c21dc123d99d622b8d40f5e88d100c13a092
Julien Combattelli <julien.combattelli@gmail.com>
2023-02-16 08:20:38 +0100

    [FindLCOV] Handle lcov version

--- cmake/Modules/FindLCOV.cmake
+++ cmake/Modules/FindLCOV.cmake
@@ -4,13 +4,26 @@ find_program(LCOV_EXECUTABLE
     NAMES lcov
     HINTS $ENV{LCOV_HOME}/bin # From chocolatey installation
 )
+
 find_program(GenHTML_EXECUTABLE
     NAMES genhtml
     HINTS $ENV{LCOV_HOME}/bin # From chocolatey installation
 )
 
+if(PERL_EXECUTABLE AND LCOV_EXECUTABLE)
+    execute_process(
+        COMMAND ${PERL_EXECUTABLE} ${LCOV_EXECUTABLE} --version
+        OUTPUT_VARIABLE LCOV_VERSION
+    )
+    string(REGEX REPLACE "^lcov: LCOV version [v]?(.*)" "\\1" LCOV_VERSION "${LCOV_VERSION}")
+    string(STRIP "${LCOV_VERSION}" LCOV_VERSION)
+endif()
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LCOV REQUIRED_VARS LCOV_EXECUTABLE GenHTML_EXECUTABLE PERL_FOUND)
+find_package_handle_standard_args(LCOV
+    REQUIRED_VARS LCOV_EXECUTABLE GenHTML_EXECUTABLE PERL_FOUND
+    VERSION_VAR LCOV_VERSION
+)
 
 if(LCOV_FOUND)
     # Preppend Perl interpreter before executable scripts
