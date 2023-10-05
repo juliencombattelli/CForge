0631e1cb390ee661f2d4678781c5453b94b0d93f
Julien Combattelli <julien.combattelli@gmail.com>
2023-10-05 08:52:34 +0200

    [DEBUG] Print filesystem on Linux

--- test/Packaging/add_subdirectory.test.cmake
+++ test/Packaging/add_subdirectory.test.cmake
@@ -6,8 +6,27 @@ to be sure to test the desired revision.
 
 #]=============================================================================]
 
+if(UNIX AND NOT APPLE)
+    execute_process(
+        COMMAND tree /home/runner/work/CForge/CForge
+    )
+    execute_process(
+        COMMAND tree /home/runner/work/CForge/cforge-build
+    )
+endif()
+
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/add_subdirectory DESTINATION ${CMAKE_CURRENT_SOURCE_DIR})
 file(COPY ${CFORGE_UNIT_PROJECT_SOURCE_DIR} DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/add_subdirectory/external)
+
+if(UNIX AND NOT APPLE)
+    execute_process(
+        COMMAND tree /home/runner/work/CForge/CForge
+    )
+    execute_process(
+        COMMAND tree /home/runner/work/CForge/cforge-build
+    )
+endif()
+
 execute_process(
     COMMAND ${CMAKE_COMMAND}
         -S ${CMAKE_CURRENT_SOURCE_DIR}/add_subdirectory
