97c6eb6bcbd3c008ba89c9b056fc1264626d163f
Julien Combattelli <julien.combattelli@gmail.com>
2024-01-04 10:58:30 +0100

    [CForgeForceColoredOutput] Force ANSI color code with Clang on Windows

--- cmake/Modules/CForgeForceColoredOutput.cmake
+++ cmake/Modules/CForgeForceColoredOutput.cmake
@@ -28,9 +28,16 @@ option(${CFORGE_PROJECT_PREFIX}_FORCE_COLORED_OUTPUT "Always produce ANSI-colore
 
 if(${${CFORGE_PROJECT_PREFIX}_FORCE_COLORED_OUTPUT})
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        # For GCC, colors can also be forced using the GCC_COLORS environment
+        # variable as explained here:
+        # https://gcc.gnu.org/onlinedocs/gcc/Diagnostic-Message-Formatting-Options.html#index-fdiagnostics-color
         add_compile_options(-fdiagnostics-color=always)
     elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
         add_compile_options(-fcolor-diagnostics)
+        if(CMAKE_HOST_WIN32)
+            # Force to use ANSI color codes on Windows
+            add_compile_options(-fansi-escape-codes)
+        endif()
     else()
         # Intentionnally do not emit a warning if ${CFORGE_PROJECT_PREFIX}_FORCE_COLORED_OUTPUT
         # was requested but the current compiler is not supported as it would be
